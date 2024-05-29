21088397e9f5bab00f99a675670d5390ddf07d93
Rimo2 <mail2asis@gmail.com>
2024-05-29 23:38:54 +0530

    Task 1

---
+++ js/js_tasks/task1.js
@@ -0,0 +1,30 @@
+/*
+## Task 1: Variables and Data Types (Simple)
+Create a JavaScript file and do the following:
+
+1. Declare a variable named `myName` and assign it your name.
+2. Declare a variable named `myAge` and assign it your age.
+3. Declare a variable named `isProgrammer` and assign it a boolean value 
+depending on whether you are a programmer or not.
+4. Log all these variables to the console.
+*/
+
+
+let myName = 'Subhasish'
+let myAge = 32
+let isProgrammer = true;
+
+console.log('My name is: '+myName)
+console.log('My age is: '+myAge)
+console.log('I am a programmer: '+isProgrammer)
+
+
+let myProfile = {
+
+"myName":"Rimo",
+"age":30,
+"isProgrammer": true
+
+}
+
+console.log('My name is: ',myProfile.myName)
---
+++ ts/ts_tasks/task1.ts
@@ -0,0 +1,19 @@
+/*
+## Task 1: Variables and Data Types (Simple)
+Create a JavaScript file and do the following:
+
+1. Declare a variable named `myName` and assign it your name.
+2. Declare a variable named `myAge` and assign it your age.
+3. Declare a variable named `isProgrammer` and assign it a boolean value 
+depending on whether you are a programmer or not.
+4. Log all these variables to the console.
+*/
+
+
+let myName2: string = 'Subha';
+let myAge2: number = 32;
+let isProgrammer2: boolean = true;
+
+console.log('My name is: ' + myName2);
+console.log('My age is: ' + myAge2);
+console.log('Am I a programmer? ' + isProgrammer2);
